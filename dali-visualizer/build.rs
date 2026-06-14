e4b3b9fda4c9e866501affbe13f97b1f91f65ff2
Muhammed Dali <muhammeddardali@users.noreply.github.com>
2026-06-14 22:03:29 +0300

    fix: github actions dali-visualizer derleme hataları için yollar ve paketler eklendi

--- dali-visualizer/build.rs
+++ dali-visualizer/build.rs
@@ -1,5 +1,12 @@
 fn main() {
     println!("cargo:rustc-link-search=native=../native-dist/linux");
+    
+    // GitHub Actions veya CI ortamı için ek arama yolları
+    if let Ok(prefix) = std::env::var("PROJECTM_PREFIX") {
+        println!("cargo:rustc-link-search=native={}/lib", prefix);
+        println!("cargo:rustc-link-search=native={}/lib/x86_64-linux-gnu", prefix);
+    }
+
     println!("cargo:rustc-link-lib=dylib=projectM-4");
     println!("cargo:rustc-env=LD_LIBRARY_PATH=../native-dist/linux");
 }
